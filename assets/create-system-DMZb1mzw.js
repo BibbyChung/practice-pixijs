b556fdef4d5f2aaba0bf14b6e0daf6eedc7a76ff
Bibby Chung <bibbynet@gmail.com>
2024-06-02 17:50:59 +0800

    Updates

---
+++ assets/create-system-DMZb1mzw.js
@@ -0,0 +1,1 @@
+import{B as t}from"./index-YpI8y5Bw.js";class o extends t{getQuery(){return this._ge.miniplexECS.with("createComponent").onEntityAdded}execute(){this.getQuery().subscribe(e=>{e.create(),this._ge.miniplexECS.removeComponent(e,"createComponent")})}}export{o as CreateSystem};
