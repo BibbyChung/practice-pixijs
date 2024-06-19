32c98249b8e40fa2aed039f7e1f798e0623d6c00
Bibby Chung <bibbynet@gmail.com>
2024-06-19 17:35:05 +0800

    refactor: rename

--- src/lib/common/utils.ts
+++ src/lib/common/utils.ts
@@ -8,6 +8,7 @@ export enum EnumContainerLabel {
   root = "root",
 }
 
+// getRandomInt(-10,10)
 export const getRandomInt = (lower: number, upper: number): number =>
   Math.floor(Math.random() * (upper - lower + 1)) + lower;
 
